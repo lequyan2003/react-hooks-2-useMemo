63fda5f8794e13e2e0fb18a2da736f43764e57d7
lequyan2003 <lequyan2003.com>
2024-06-04 15:56:26 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react'
+
+function expensiveFunction(number) {
+  console.log('Start');
+  const start = new Date();
+
+  // Wait for 3s
+  while (new Date() - start < 3000);  // 3s = 3000ms
+
+  console.log('End', new Date() - start, 'ms');
+  
+  return number * number;
+}
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(20);
+
+  const number = useMemo(() => {
+    return expensiveFunction(num);
+  }, [num]);  // if num changes, re-execute func in useMemo
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+
+      <p>Number: {number}</p>
+    </>
   );
 }
 
